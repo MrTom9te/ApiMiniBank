d6491677f2d9730ab9cabf2dfe2fa8258059ab6f
mr_tomate <gabriel_santarem@outlook.com>
2025-09-02 07:40:53 -0400

    Create routes cofig

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-use actix_web::{
-    App,
-    dev::{Service, ServiceFactory},
-};
+use actix_web::web::{self, ServiceConfig};
+
+use crate::handlers::auth_routes;
 
 mod database;
+mod handlers;
 mod models;
+
+pub fn app(cgf: &mut ServiceConfig) {
+    cgf.service(web::scope("/api").service(web::scope("/v1").configure(auth_routes)));
+}
