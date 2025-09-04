3e89b3acd9bd3b0bac69a18e715dad6f47f14299
mr_tomate <gabriel_santarem@outlook.com>
2025-09-04 15:03:57 -0400

    criaçaõ de variavel global JWT_SECRET

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
-use actix_web::web::{self, ServiceConfig};
+use std::sync::OnceLock;
 
 use crate::handlers::auth_routes;
+use actix_web::web::{self, ServiceConfig};
 
 mod database;
 mod handlers;
+pub mod middleware;
 mod models;
 mod utils;
+pub mod validators;
+
+pub static JWT_SECRET: OnceLock<String> = OnceLock::new();
+
 pub fn app(cgf: &mut ServiceConfig) {
     cgf.service(web::scope("/api").service(web::scope("/v1").configure(auth_routes)));
 }
