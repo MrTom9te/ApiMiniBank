acf0a9ed6aee607493dfa76c2aff33d139739fb7
mr_tomate <gabriel_santarem@outlook.com>
2025-09-04 15:02:54 -0400

    criação do enpoint login

--- src/handlers/users.rs
+++ src/handlers/users.rs
@@ -1,3 +1,5 @@
+use std::any::Any;
+
 use actix_web::{
     HttpResponse, Responder, post,
     web::{self, Data, Json, ServiceConfig},
@@ -6,7 +8,12 @@ use sqlx::PgPool;
 
 use crate::{
     database::UserRepository,
-    models::{CreateUser, User, api_response::ApiResponse},
+    models::{
+        CreateUser, LoginUserRequest, LoginUserResponse, User, api_response::ApiResponse,
+        error::UserError,
+    },
+    utils::{create_jwt, verify_password},
+    validators::LoginValidator,
 };
 
 #[post("/register")]
@@ -32,6 +39,61 @@ async fn create_user(pool: Data<PgPool>, Json(create_user): Json<CreateUser>) ->
     }
 }
 
+#[post("/login")]
+pub async fn login_user(pool: Data<PgPool>, login: Json<LoginUserRequest>) -> impl Responder {
+    let validated_login = match LoginValidator::validate_login_data(&login.email, &login.password) {
+        Ok(data) => data,
+        Err(_) => {
+            return HttpResponse::BadRequest().json(ApiResponse::<()>::error(
+                "INVALID INPUT",
+                "Dados de login inválidos",
+            ));
+        }
+    };
+
+    let user = match UserRepository::find_by_email(&pool, &validated_login.email).await {
+        Ok(Some(user)) => user,
+        Ok(None) => {
+            return HttpResponse::Unauthorized().json(ApiResponse::<()>::error(
+                &UserError::InvalidCredentials.to_string(),
+                "Email ou senha incorretos",
+            ));
+        }
+        Err(_) => {
+            return HttpResponse::InternalServerError().json(ApiResponse::<()>::error(
+                &UserError::InvalidCredentials.to_string(),
+                "Erro interno do servidor",
+            ));
+        }
+    };
+
+    if !verify_password(&validated_login.password, &user.password_hash) {
+        return HttpResponse::Unauthorized().json(ApiResponse::<()>::error(
+            &UserError::InvalidCredentials.to_string(),
+            "Email ou senha incorretos",
+        ));
+    }
+
+    let token = match create_jwt(&user) {
+        Ok(token) => token,
+        Err(_) => {
+            return HttpResponse::InternalServerError().json(ApiResponse::<()>::error(
+                "FAILED CREATE TOKEN",
+                "Erro interno do servidore",
+            ));
+        }
+    };
+
+    HttpResponse::Ok().json(ApiResponse::sucess(
+        LoginUserResponse {
+            token,
+            user_id: user.id,
+            email: user.email,
+        },
+        "login efetuado com sucesso",
+    ))
+}
+
 pub fn auth_routes(cfg: &mut ServiceConfig) {
-    cfg.service(web::scope("/auth").service(create_user));
+    cfg.service(web::scope("/auth").service(create_user).service(login_user));
 }
